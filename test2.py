bf5b5450d77881aa8e60b1584625973287db4a49
explorall <63055487+explorall@users.noreply.github.com>
2023-11-17 11:35:13 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -17,10 +17,8 @@ hist_quantiles = {
     for quantile in quantiles
 }
 
-"""
 fig, ax = plt.subplots()
 ax.plot(X_1d, y, "o", alpha=0.5, markersize=1)
 for quantile, hist in hist_quantiles.items():
     ax.plot(X_1d, hist.predict(X), label=quantile)
 _ = ax.legend(loc="lower left")
-"""
