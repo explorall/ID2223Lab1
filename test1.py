378286e3a4475a48689d9742242bda4881b20737
explorall <63055487+explorall@users.noreply.github.com>
2023-11-17 11:25:06 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,9 +1,10 @@
-#from math import *
+import numpy as np
 
 def test1func():
   print("test1func")
 
 if __name__ == "__main__":
   print("From main test 1")
+  print(np.arange(9).reshape((3,3)))
 else:
   print("Not main test 1")
